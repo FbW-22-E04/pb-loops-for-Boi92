2e49af121b5317da0ac989a9ae407c474d473b3b
Boi Schrader <boi1992@t-online.de>
2022-07-12 13:16:21 +0200

    done for now until 5, need a break

---
+++ solution.js
@@ -0,0 +1,23 @@
+let sum = 0;
+for(i=1;i<21;i++)
+{
+    sum+=i;
+}
+console.log(sum);
+
+for(i=1;i<6;i++)
+{
+    console.log("There are ",i," bottles of beer on the table.")
+}
+
+for(i=0;i<21;i++)
+{
+    if(i % 2 == 0)console.log(i,"is even");
+    else console.log(i,"is odd")
+}
+
+for (i=0;i<11;i++)
+{
+    console.log(i,"* 9 =", i*9);
+}
+
